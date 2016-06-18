7f205a30892f2a41e3e4767b14daa7db126964c0
zhugeyicixin <hetanjin@163.com>
2016-06-18 21:25:42 +0800

    abinitio.ver.2.1.20
    correct the thermodynamics
    mainly about the cyclic correction
    add the transport data

---
+++ CANTERA/cheminp2xml.bat
@@ -0,0 +1,3 @@
+
+python ck2cti.py --input=farnesane_abinitio_withoutNegativeRate.inp --thermo=farnesane_estimation3.dat --transport=farnesane_transport.dat --id='farnesane' --permissive
+cti2ctml.exe farnesane_abinitio_withoutNegativeRate.cti
---
+++ THERM/Farnesane/dataIn2016/_2_autoFit.bat
@@ -0,0 +1,2 @@
+@echo off
+python _2_autoFit.py>autoThermLog.txt
